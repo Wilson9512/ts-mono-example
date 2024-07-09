dce635d2955eccacea1c4f349928156b136f471c
Wilson9512 <cwhth2956@gmail.com>
2024-07-09 16:10:29 +0800

    build(core): Init react-native use common function.

---
+++ packages/app1/metro.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../node_modules/@ts-monorepo/common'),
+  ],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
